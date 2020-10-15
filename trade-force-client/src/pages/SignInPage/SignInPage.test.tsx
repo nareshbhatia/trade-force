102156274cff02eff93619ded1e7336b54b166fa
Naresh Bhatia <naresh.archfirst@gmail.com>
2020-10-15 17:09:39 -0400

    test: added SignInPage test

---
+++ trade-force-client/src/pages/SignInPage/SignInPage.test.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { useUsers } from '../../hooks';
+import { SignInPage } from './SignInPage';
+
+jest.mock('../../hooks/useUsers', () => ({
+    useUsers: jest.fn(),
+}));
+
+const mockedUseUsers = useUsers as jest.Mocked<typeof useUsers>;
+
+// ----- Test -----
+describe('SignInPage', () => {
+    it('shows a list of users', async () => {
+        mockedUseUsers.mockReturnValue({
+            isLoading: false,
+            isError: false,
+            data: [
+                {
+                    id: 'elvis.presley',
+                    displayName: 'Elvis Presley',
+                    initials: 'EP',
+                    role: 'pm',
+                },
+                {
+                    id: 'paul.mccartney',
+                    displayName: 'Paul McCartney',
+                    initials: 'PM',
+                    role: 'pa',
+                },
+                {
+                    id: 'tom.cruise',
+                    displayName: 'Tom Cruise',
+                    initials: 'TC',
+                    role: 'trader',
+                },
+            ],
+        });
+        const { findAllByTestId } = render(<SignInPage />);
+        const userRows = await findAllByTestId('user-row');
+        expect(userRows.length).toBe(3);
+    });
+});
